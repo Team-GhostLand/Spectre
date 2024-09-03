95f20a0d827102b0207a18c2eae5c8e08e4f692d
guzio <star.master.123.mg@gmail.com>
2024-09-03 15:51:40 +0200

    pain

--- universal-installer-scaffolding.sh
+++ universal-installer-scaffolding.sh
@@ -102,7 +102,7 @@ if [ "$STRATEGY" == "ARCHIVE" ]; then
             echo "or remember to do \`chmod 555 $(pwd)\` if you use Ctrl+C.";
             while [ ! -e "$PROJECT_NAME.zip" ]; do
                 read -r -n 1 -s -t 1;
-                echo "$?";
+                echo "Read timed out. Is said: $?";
                 if [ $? -eq 0 ]; then
                     chmod --verbose 555 .;
                     echo "NOTE: Safely cancelled.";
